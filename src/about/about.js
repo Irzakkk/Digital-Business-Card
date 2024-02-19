1dc4b6c38f9731b0da1320b83719f09e62a3fa93
Irzakkk <nkhajawaal@gmail.com>
2024-02-19 17:44:32 +0530

    Initial commit

---
+++ src/about/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './about.css'
+
+export default function About(){
+    return(
+        <div className='about-div'>
+         <h3>About</h3>  
+         <p>An Undergrad who still haven't figured out what he wants from his life.
+            A guy with shitloads of dreams and a glass of water.
+            </p> 
+         <h3>Interests</h3>  
+         <p> Anime, Jim, Read, React :x, Youtube, Detox, Caffeine, Dumbell,  
+            </p> 
+        </div>
+    )
+}
---
+++ src/info/info.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './info.css'
+// import { ReactDOM, render } from 'react-dom'
+import photo from './profile-picture.jpg'
+export default function Info(){
+        return (
+            <div className='info-div'>
+                <img src={photo}></img>
+                <h1>Irzak Khajawaal</h1>
+                <p>Frontend Developer</p>
+                <p className='web'>irzak.me</p>
+                <div className='btn-div'>
+                <button>
+                <svg className='email-logo' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16"> <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/> </svg>
+                    Email</button>
+                <button>
+                <svg className='linkedIn-logo' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+                    LinkedIn</button>
+                </div>
+            </div>
+
+        );
+}
